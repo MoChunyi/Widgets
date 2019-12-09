fe374a00dcb4be32ac394acdfad7da4011a26965
chunyimo <chunyi.mo@yitu-inc.com>
2019-12-09 10:47:58 +0800

    Updates

---
+++ precache-manifest.9307a8032d98f3466194455eda261e25.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0956f33d50893ca977940e0fc585373b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8181acb403eca2a528d1",
+    "url": "./static/js/0.01bcbd9e.chunk.js"
+  },
+  {
+    "revision": "872abca32580b090da93",
+    "url": "./static/js/3.4703d5b9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/3.4703d5b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10cad932fb33ee1f72e3",
+    "url": "./static/js/4.d3b7659b.chunk.js"
+  },
+  {
+    "revision": "ba2c86f52ea091e7bb7474e23553c8a5",
+    "url": "./static/js/4.d3b7659b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35b2a1ab0e5f6fde394c",
+    "url": "./static/js/5.490afba9.chunk.js"
+  },
+  {
+    "revision": "fc61d73f6909c61e2510",
+    "url": "./static/js/6.3258ff20.chunk.js"
+  },
+  {
+    "revision": "3aeed7873244977ef4f5",
+    "url": "./static/js/main.7012b162.chunk.js"
+  },
+  {
+    "revision": "d8b58ee73585b5ecdf7b",
+    "url": "./static/js/runtime-main.cd59c590.js"
+  }
+]);
